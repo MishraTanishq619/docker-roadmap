9a2c46edf73aa70fc20035861d4403393c0edbb6
Tanishq Mishra <mishratanishq619@gmail.com>
2024-03-13 10:30:31 +0530

    myCommits

--- part-2/1-complex-app/index.js
+++ part-2/1-complex-app/index.js
@@ -1,30 +1,63 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
 // Set up mongoose connection
-mongoose.connect("mongodb://mongdb2:27017/mydatabase", {});
+mongoose.connect("mongodb://mongo_db/db2", {});
 
 const EntrySchema = new mongoose.Schema({
-  text: String,
-  date: { type: Date, default: Date.now },
+	text: String,
+	date: { type: Date, default: Date.now },
 });
 
-const Entry = mongoose.model('Entry', EntrySchema);
+const Entry = mongoose.model("Entry", EntrySchema);
 
-app.get('/', async (req, res) => {
-  try {
-    const entry = new Entry({ text: 'This is an entry by harkirat' });
-    await entry.save();
-    res.send('Entry added!');
-  } catch (err) {
-    res.status(500).send('Error occurred');
-  }
+app.get("/", async (req, res) => {
+	res.send("Hello World!.");
+});
+
+app.get("/one", async (req, res) => {
+	try {
+		const entry = new Entry({ text: "This is an entry by harkirat" });
+		await entry.save();
+		res.send("Entry added!");
+	} catch (err) {
+		res.status(500).send("Error occurred");
+	}
+});
+
+app.get("/two", async (req, res) => {
+	try {
+		const entry = new Entry({ text: "This is by tarzan" });
+		await entry.save();
+		res.send("Entry added!");
+	} catch (err) {
+		res.status(500).send("Error occurred");
+	}
+});
+
+app.get("/three", async (req, res) => {
+	try {
+		const entry = new Entry({ text: "This is  harkirat" });
+		await entry.save();
+		res.send("Entry added!");
+	} catch (err) {
+		res.status(500).send("Error occurred");
+	}
+});
+
+app.get("/four", async (req, res) => {
+	try {
+		const entry = new Entry({ text: "Hello world." });
+		await entry.save();
+		res.send("Entry added!");
+	} catch (err) {
+		res.status(500).send("Error occurred");
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
-
--- part-2/5-docker-compose/index.js
+++ part-2/5-docker-compose/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
@@ -9,23 +9,22 @@ console.log("hi from the backend application");
 mongoose.connect("mongodb://mongodb/testdb", {});
 
 const EntrySchema = new mongoose.Schema({
-  text: String,
-  date: { type: Date, default: Date.now },
+	text: String,
+	date: { type: Date, default: Date.now },
 });
 
-const Entry = mongoose.model('Entry', EntrySchema);
+const Entry = mongoose.model("Entry", EntrySchema);
 
-app.get('/', async (req, res) => {
-  try {
-    const entry = new Entry({ text: 'This is an entry' });
-    await entry.save();
-    res.send('Entry added!');
-  } catch (err) {
-    res.status(500).send('Error occurred');
-  }
+app.get("/", async (req, res) => {
+	try {
+		const entry = new Entry({ text: "This is an entry" });
+		await entry.save();
+		res.send("Entry added!");
+	} catch (err) {
+		res.status(500).send("Error occurred");
+	}
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
-
